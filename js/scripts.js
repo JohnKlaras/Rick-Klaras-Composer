305985f675f8aec39a33b531475a7ebf8fd8adc4
Rick Klaras <john.r.klaras@gmail.com>
2016-06-28 00:10:38 -0700

    add more playlists

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 
-
+// Hide controls until hover
 $('#video1, #video2, #video3').hover(function toggleControls() {
     if (this.hasAttribute("controls")) {
         this.removeAttribute("controls")
